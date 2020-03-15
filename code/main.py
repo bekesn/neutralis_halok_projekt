508d11e27582e01cf10a19716b8a07dcfca99061
Golyo <david9849@freemail.hu>
2020-03-15 13:55:13 +0100

    sebesseg kezelese, almost not real physics

--- code/main.py
+++ code/main.py
@@ -1,27 +1,38 @@
 import physics
+import physics
 import kirajzolas
 import perception
 import pygame
 
-t = kirajzolas.getTracks(1)
+t = kirajzolas.getTracks(2)
 x = t.startPos[0]
 y = t.startPos[1]
 Dir = t.startDir
+speed = 0.0     # A sebesség változtatásához
+skid = 0.0  # A csúszáshoz
+skid_factor = 5     # Megadja, mennyivel változzon a csúszás értéke
 kirajzolas.palya_1k=t.outer
 kirajzolas.palya_1b=t.inner
 
 
 def manual_steering():
     turn = 0.0
+    dspeed = 0.0
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_LEFT]:
-        turn = 0.3
+        turn = 0.4
     if pressed[pygame.K_RIGHT]:
-        turn = -0.3
-    return turn
+        turn = -0.4
+    if pressed[pygame.K_UP]:
+        dspeed = 0.01   # Sebesség növelése
+    if pressed[pygame.K_DOWN]:
+        dspeed = -0.01  # Sebesség csökkentése
+    return turn, dspeed
 
 while True:
-    (x, y, Dir) = physics.move(x, y, Dir, 0.5, manual_steering())
+    speed = speed + manual_steering()[1]    # Tényleges sebesség kiszámítása
+    (x, y, Dir, skid) = physics.move(x, y, Dir, speed, manual_steering()[0], skid)
+    skid = skid + speed * skid_factor   # Csúszás értékének meghatározása a sebesség függvényében
     perception.calcDistances(x, y, Dir)
     kirajzolas.drawPalya(x, y, Dir)
 
--- code/physics.py
+++ code/physics.py
@@ -1,34 +1,49 @@
 import math
 
-#constants describing physics
-wheelBase=20
-track=10
-slipEnabled=True
-slipLimit=0.004
-turnLimit=0.5
+# constants describing physics
+wheelBase = 20
+track = 10
+slipEnabled = True
+slipLimit = 0.004
+turnLimit = 0.3   # math.pi/4
+g = 9.81
+mu = 0.8  # Tapadási súrlódási tényező
+px = 5  # px/s to m/s
+pr = 10  # m to px
 
-#status
-slip=False
-collision=False
+# status
+slip = False
+collision = False
 
-def move(x,y,dir,forward,dirChange):
-    if dirChange==0:
-        x=x+math.sin(dir)*forward
-        y=y+math.cos(dir)*forward
+
+def move(x, y, dir, forward, dirChange, skid):
+    if dirChange == 0:
+        x = x + math.sin(dir) * forward
+        y = y + math.cos(dir) * forward
+        skid = 0.0  # Ebben az esetben nem csúszik az autó
     else:
         dirChange = min(dirChange, turnLimit)
         dirChange = max(dirChange, -turnLimit)
-        rad=wheelBase/math.atan(dirChange)
-        rad=math.sqrt(rad*rad+wheelBase*wheelBase/4)*dirChange/abs(dirChange)
-        origo=(x+math.cos(dir)*rad,y-math.sin(dir)*rad)
-        if slipEnabled and abs(forward * forward / rad) > slipLimit:
+        rad = wheelBase / math.tan(dirChange)
+        rad = math.sqrt(rad * rad + wheelBase * wheelBase / 4) * dirChange / abs(dirChange)
+        origo = (x + math.cos(dir) * rad, y - math.sin(dir) * rad)
+        v_max = math.sqrt(g * mu * abs(rad / pr))
+        rad_min = forward * forward * px * px * pr / (g * mu)
+        if slipEnabled and forward * px > v_max:
             slip = True
+            if abs(rad) + skid < rad_min:   # Addig növeljük a csúszás értékét, amíg a megfelelő körpályára kerül
+                mrad = rad / (abs(rad) + skid)
+            else:
+                mrad = rad / rad_min
+                skid = rad_min - abs(rad) + 10  # Amint a megfelelő körpályán vagyunk, nem változik az értéke. 10 := biztonsági tényező
+            # print(forward, v_max, rad, rad_min, skid, mrad)
             x = x + math.sin(dir) * forward
             y = y + math.cos(dir) * forward
-            dir=dir+forward/rad/3
+            dir = dir + forward / rad * abs(mrad)   # Ezzel a képlettel adjuk meg a csúszás mértékét
         else:
-            slip=False
-            dir=dir+forward/rad
-            x=origo[0]-math.cos(dir)*rad
-            y=origo[1]+math.sin(dir)*rad
-    return (x,y,dir)
+            slip = False
+            skid = 0.0  # Ebben az esetben nem csúszik az autó
+            dir = dir + forward / rad
+            x = origo[0] - math.cos(dir) * rad
+            y = origo[1] + math.sin(dir) * rad
+    return (x, y, dir, skid)
